0c313b92646b3af570954201dc38606fe465d031
Tassilo <tassia710@gmail.com>
2020-10-16 19:33:43 +0200

    Fixed color conversion

--- lua/plugins/string_utility/sh_color.lua
+++ lua/plugins/string_utility/sh_color.lua
@@ -13,19 +13,13 @@
 -- @returns c [Color] - the generated color
 -- @since v1.0.0
 function string.ToColor(str)
-	if string.len(str) == 8 then
-		local i = string.HexToNumber(str)
-		if not i then return nil end
-		local r = bit.band(bit.rshift(i, 24), 0xff)
-		local g = bit.band(bit.rshift(i, 16), 0xff)
-		local b = bit.band(bit.rshift(i, 8), 0xff)
-		local a = bit.band(i, 0xff)
-		return Color(r, g, b, a)
-	elseif string.len(str) == 6 then
-		return string.ToColor(str .. "ff")
-	else
-		return nil
-	end
+	local i = string.HexToNumber(str)
+	if not i then return nil end
+	local r = bit.band(bit.rshift(i, 24), 0xff)
+	local g = bit.band(bit.rshift(i, 16), 0xff)
+	local b = bit.band(bit.rshift(i, 8), 0xff)
+	local a = bit.band(i, 0xff)
+	return Color(r, g, b, a)
 end
 
 
@@ -40,10 +34,7 @@ function string.FromColor(c)
 	i = bit.bor(i, c.g)
 	i = bit.lshift(i, 8)
 	i = bit.bor(i, c.b)
-	if c.a ~= 255 then
-		i = bit.lshift(i, 8)
-		i = bit.bor(i, c.a)
-	end
-	print(i)
+	i = bit.lshift(i, 8)
+	i = bit.bor(i, c.a)
 	return string.NumberToHex(i)
 end
