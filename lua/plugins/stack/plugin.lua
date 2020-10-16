2c6f66b6e8d7da1e88412b0f384263b3afc4a26b
Tassilo <tassia710@gmail.com>
2020-10-16 17:09:54 +0200

    Renamed plugin.json
    plugin.json -> plugin.lua

---
+++ lua/plugins/stack/plugin.lua
@@ -0,0 +1,11 @@
+return {
+	["author"] = "TASSIA710",
+	["name"] = "Stack",
+	["description"] = "Adds a nice stack with a couple of features.",
+	["version"] = "1.0.0",
+	["disabled"] = false,
+	["dependencies"] = {
+	},
+	["soft_dependencies"] = {
+	}
+}
