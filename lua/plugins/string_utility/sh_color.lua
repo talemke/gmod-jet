1b5c8afb98bf363c34846bdfa80469e37bd379b8
Tassilo <tassia710@gmail.com>
2020-10-16 18:29:30 +0200

    Added string.ToColor

--- lua/plugins/string_utility/sh_color.lua
+++ lua/plugins/string_utility/sh_color.lua
@@ -13,7 +13,19 @@
 -- @returns c [Color] - the generated color
 -- @since v1.0.0
 function string.ToColor(str)
-	-- TODO
+	if string.len(str) == 8 then
+		local i = string.HexToNumber(str)
+		if not i then return nil end
+		local r = bit.band(bit.rshift(i, 24), 0xff)
+		local g = bit.band(bit.rshift(i, 16), 0xff)
+		local b = bit.band(bit.rshift(i, 8), 0xff)
+		local a = bit.band(i, 0xff)
+		return Color(r, g, b, a)
+	elseif string.len(str) == 6 then
+		return string.ToColor(str .. "ff")
+	else
+		return nil
+	end
 end
 
 
@@ -28,5 +40,10 @@ function string.FromColor(c)
 	i = bit.bor(i, c.g)
 	i = bit.lshift(i, 8)
 	i = bit.bor(i, c.b)
+	if c.a ~= 255 then
+		i = bit.lshift(i, 8)
+		i = bit.bor(i, c.a)
+	end
+	print(i)
 	return string.NumberToHex(i)
 end
