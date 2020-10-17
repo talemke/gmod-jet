48123d764ca6dc98975431f4b6f19abbe862eee4
Tassilo <tassia710@gmail.com>
2020-10-17 12:29:53 +0200

    Improved help command

--- lua/plugins/cli/commands/sv_help.lua
+++ lua/plugins/cli/commands/sv_help.lua
@@ -7,13 +7,27 @@
 --]]
 
 jet.AddCliCommand("help", function(args)
-	print("-------------------")
-	print("Available commands:")
+	-- Analyze first
+	local commands = {}
+	local maxCommandWidth = 0
+	local maxDescriptionWidth = 0
 
 	for _, cmd in ipairs(jet.GetCliCommands()) do
-		local usage = Either(cmd.usage ~= nil, cmd.usage, "")
-		print("jet " .. cmd.name .. " " .. usage .. " | " .. cmd.description)
+		local command = string.Trim("jet " .. cmd.name .. " " .. (cmd.usage or ""))
+		maxCommandWidth = math.max(maxCommandWidth, string.len(command))
+		maxDescriptionWidth = math.max(maxDescriptionWidth, string.len(cmd.description))
+		table.insert(commands, {
+			command, cmd.description
+		})
 	end
 
-	print("-------------------")
+	local maxTotalWidth = maxCommandWidth + maxDescriptionWidth + 3
+	local maxDashWidth = math.ceil((maxTotalWidth - 12) / 2)
+	local dashes = string.Repeating(maxDashWidth, "-")
+
+	print(dashes .. " Start Help " .. dashes)
+	for _, cmd in ipairs(commands) do
+		print(cmd[1] .. string.Repeating(maxCommandWidth - string.len(cmd[1]), " ") .. " | " .. cmd[2])
+	end
+	print(dashes .. "- End Help -" .. dashes)
 end, "", "Shows this help.")
