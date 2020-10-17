c02376462aca34f812502913a68c00fdcb6ef6c9
Tassilo <tassia710@gmail.com>
2020-10-17 23:11:37 +0200

    Added jet.ParseDependency

--- lua/jet/sh_utility.lua
+++ lua/jet/sh_utility.lua
@@ -12,12 +12,33 @@
 -- @param str [string] - the version string (e.g. '1.0.0')
 -- @returns version [table] - the version data, or `nil`
 function jet.ParseVersion(str)
+	if str[1] == "v" then str = string.sub(str, 2) end
 	local parsed = {string.match(str, "([%d])%.([%d])%.([%d])")}
 	if not parsed[1] and not parsed[2] and not parsed[3] then
 		return nil
 	else
 		return parsed
 	end
+end
+
+
+
+--- Parses a dependency string.
+-- @param str [string] - the dependency string
+-- @returns dependency [table] - dependency data, or `nil`
+function jet.ParseDependency(str)
+	str = string.Explode("/", str)
+	if #str ~= 2 then return nil end
+	local author = str[1]
+	local name = string.Explode("@", str[2])
+	if #name ~= 2 then return nil end
+	local version = jet.ParseVersion(name[2])
+	if not version then return nil end
+	return {
+		author = author,
+		name = name[1],
+		version = version
+	}
 end
 
 
