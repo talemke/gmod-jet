bef79ca10c47340412af0819d429a7e1067f1725
Tassilo <tassia710@gmail.com>
2020-11-21 00:12:57 +0100

    Added OpenURL

--- lua/plugins/generic/cl_player.lua
+++ lua/plugins/generic/cl_player.lua
@@ -13,3 +13,9 @@ local PLAYER = FindMetaTable("Player")
 function PLAYER:Notify(text, class, length)
 	notification.AddLegacy(text, class or NOTIFY_GENERIC, length or 5)
 end
+
+
+
+function PLAYER:OpenURL(url)
+	gui.OpenURL(url)
+end
