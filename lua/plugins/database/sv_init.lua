de00e0265ad2602c22c29a8ff0cf3027bd5d952e
Tassilo <tassia710@gmail.com>
2020-11-23 22:52:52 +0100

    Update sv_init.lua

--- lua/plugins/database/sv_init.lua
+++ lua/plugins/database/sv_init.lua
@@ -7,6 +7,14 @@
 --]]
 
 
+-- Configuration
+config.AddPlugin("TASSIA710/Database", "Hostname", "localhost", "string", "The hostname of the database.")
+config.AddPlugin("TASSIA710/Database", "Port", 3306, "number", "The port of the database.")
+config.AddPlugin("TASSIA710/Database", "Database", "database", "string", "The database name.")
+config.AddPlugin("TASSIA710/Database", "Username", "username", "string", "The username to use for logging in.")
+config.AddPlugin("TASSIA710/Database", "Password", "password", "string", "The password to use for logging in.")
+
+
 -- Require MySQLOO
 require("mysqloo")
 
