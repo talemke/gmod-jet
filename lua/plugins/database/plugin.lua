0483969a5e55e1ce93907f0556aab3c3b2dbfb15
Tassilo <tassia710@gmail.com>
2020-11-23 22:47:31 +0100

    Update plugin.lua

--- lua/plugins/database/plugin.lua
+++ lua/plugins/database/plugin.lua
@@ -2,5 +2,8 @@ return {
 	author = "TASSIA710",
 	name = "Database",
 	description = "Goal of this plugin is to allow for global database connections, instead of each addon using their own.",
-	version = "1.0.0"
+	version = "1.0.0",
+	dependencies = {
+		"TASSIA710/Config"
+	}
 }
