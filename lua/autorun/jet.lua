0f88b65b088e21f0f28314e095ebb5e4c4c54675
Tassilo <tassia710@gmail.com>
2021-04-05 16:21:41 +0200

    Update jet.lua

--- lua/autorun/jet.lua
+++ lua/autorun/jet.lua
@@ -2,3 +2,12 @@
 	Jet - (c) 2021 Tassilo <https://tassia.net>
 	Licensed under the MIT License.
 --]]
+
+if SERVER then
+	AddCSLuaFile("../jet/client.lua")
+	include("../jet/server.lua")
+end
+
+if CLIENT then
+	include("../jet/client.lua")
+end
