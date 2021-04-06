ec53b4858ee24d603eec7ccb437514e59298e7fc
Tassilo <tassia710@gmail.com>
2021-04-06 17:36:43 +0200

    Create file.lua

---
+++ lua/jet/libraries/file.lua
@@ -0,0 +1,24 @@
+--[[
+	Jet - (c) 2021 Tassilo <https://tassia.net>
+	Licensed under the MIT License.
+--]]
+
+
+
+--- Searches for all files in the pathName, matching the pattern recursively.
+--- That is, it also searches all sub folders of the original pathName.
+---
+--- @param pathName string the path to search in
+--- @param pattern string the files to search for
+--- @param pattern string the path (e.g. `"LUA"`)
+--- @param consumer fun(dir:string,file:string) the consumer
+---
+_G.file.FindRecursive = function(pathName, pattern, path, consumer)
+	local files, dirs = file.Find(pathName .. pattern, path)
+	for _, file in ipairs(files) do
+		consumer(pathName, file)
+	end
+	for _, dir in ipairs(dirs) do
+		file.FindRecursive(pathName .. dir .. "/", pathName, path, consumer)
+	end
+end
