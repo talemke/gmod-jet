34849f845e437c523b85ef04c62ddab1ae7cdbff
Tassilo <tassia710@gmail.com>
2021-04-06 19:29:24 +0200

    Added stack plugin

---
+++ lua/plugins/stack.lua
@@ -0,0 +1,20 @@
+--[[
+	Stack Plugin - (c) 2021 Tassilo <https://tassia.net>
+	Licensed under the MIT License.
+--]]
+
+return {
+
+	GroupID = "net.tassia",
+	PluginID = "stack",
+	Version = "1.0.0",
+	Name = "Stack",
+	Description = "Adds a stack data structure.",
+	Authors = { "Tassilo" },
+
+	Homepage = "https://github.com/TASSIA710/Jet#readme",
+	License = "MIT",
+	Bugs = "https://github.com/TASSIA710/Jet/issues",
+	Repository = "https://github.com/TASSIA710/Jet.git",
+
+}
---
+++ lua/plugins/stack/classes/Stack.lua
@@ -0,0 +1,94 @@
+--[[
+	Jet - (c) 2021 Tassilo <https://tassia.net>
+	Licensed under the MIT License.
+--]]
+
+
+
+--- A stack.
+---
+--- @class Stack
+---
+local CLASS = {}
+CLASS.__index = CLASS
+
+--- @type any[]
+CLASS._Data = {}
+
+
+
+--- Returns the size of this stack.
+---
+--- @return number the size
+---
+function CLASS:Size()
+	return #self._Data
+end
+
+
+
+--- Pops the top element from the stack and returns it.
+---
+--- @return any|nil the top element
+---
+function STACK:Pop()
+	local element = self._Data[#self._Data]
+	table.remove(self._Data)
+	return element
+end
+
+
+
+--- Peeks at the top of the stack.
+---
+--- @return any|nil the top element
+function STACK:Peek()
+	return self._Data[#self._Data]
+end
+
+
+
+--- Pushes an element to the top of the stack.
+---
+--- @param element any the element
+---
+function STACK:Push(element)
+	table.insert(self._Data, element)
+end
+
+
+
+--- Clears the stack.
+---
+function STACK:Clear()
+	self._Data = {}
+end
+
+
+
+--- Removes an element from the stack.
+---
+--- @param element any the element to remove
+---
+function STACK:Remove(element)
+	table.RemoveByValue(self._Data, element)
+end
+
+
+
+--- Removes all elements from the stack where 'predicate' returns true.
+---
+--- @param predicate fun(element:any):boolean the predicate
+---
+function STACK:RemoveIf(predicate)
+	for index, element in pairs(self._Data) do
+		if predicate(element) then
+			table.remove(self._Data, index)
+		end
+	end
+end
+
+
+
+-- Register class.
+debug.getregistry()["Jet:Stack"] = CLASS
