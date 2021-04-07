855ae7043d4225230eb4bb2e36bdd2aee4563d14
Tassilo <tassia710@gmail.com>
2021-04-07 20:44:33 +0200

    Update PluginManager.lua

--- lua/jet/classes/PluginManager.lua
+++ lua/jet/classes/PluginManager.lua
@@ -343,9 +343,7 @@ end
 ---
 function CLASS:DownloadSharedFiles(info)
 	file.FindRecursive("plugins/" .. info.FolderName .. "/", "*.lua", "LUA", function(dir, file)
-		print(dir .. file)
 		if self:IsSharedFile(file) then
-			print("SHARED", dir .. file)
 			AddCSLuaFile(dir .. file)
 		end
 	end)
@@ -357,7 +355,6 @@ end
 function CLASS:DownloadClientFiles(info)
 	file.FindRecursive("plugins/" .. info.FolderName .. "/", "*.lua", "LUA", function(dir, file)
 		if self:IsClientFile(file) then
-			print("CLIENT", dir .. file)
 			AddCSLuaFile(dir .. file)
 		end
 	end)
