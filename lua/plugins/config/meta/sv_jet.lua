2aa7108dc3512bc9562b3f81ba25972c376d9264
Tassilo <tassia710@gmail.com>
2021-04-08 17:03:08 +0200

    Create sv_jet.lua

---
+++ lua/plugins/config/meta/sv_jet.lua
@@ -0,0 +1,21 @@
+--[[
+	Config Plugin - (c) 2021 Tassilo <https://tassia.net>
+	Licensed under the MIT License.
+--]]
+
+
+--- @type Jet
+local META = debug.getregistry()["Jet"]
+
+--- @type Configuration
+META._Config = nil
+
+
+
+--- Returns the global configuration.
+---
+--- @return Configuration
+---
+function META:Config()
+	return self._Config
+end
