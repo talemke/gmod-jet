37f9a62c61acb4538ebbd4c539199508ebd82ad6
Tassilo <tassia710@gmail.com>
2021-04-10 18:28:30 +0200

    Update sv_Adapter.lua

--- lua/plugins/database/classes/sv_Adapter.lua
+++ lua/plugins/database/classes/sv_Adapter.lua
@@ -20,6 +20,61 @@ function CLASS:Prepare(sql)
 end
 
 
+--- @return boolean
+function CLASS:IsConnected()
+	error("Function DatabaseAdapter::IsConnected has not been implemented.")
+end
+
+
+function CLASS:Disconnect()
+	error("Function DatabaseAdapter::Disconnect has not been implemented.")
+end
+
+
+
+
+
+--- @param stmt PreparedStatement
+--- @param index number
+function CLASS:StatementSetNull(stmt, index)
+	error("Function DatabaseAdapter::StatementSetNull has not been implemented.")
+end
+
+
+--- @param stmt PreparedStatement
+--- @param index number
+--- @param value boolean
+function CLASS:StatementSetBoolean(stmt, index, value)
+	error("Function DatabaseAdapter::StatementSetBoolean has not been implemented.")
+end
+
+
+--- @param stmt PreparedStatement
+--- @param index number
+--- @param value number
+function CLASS:StatementSetNumber(stmt, index, value)
+	error("Function DatabaseAdapter::StatementSetNumber has not been implemented.")
+end
+
+
+--- @param stmt PreparedStatement
+--- @param index number
+--- @param value string
+function CLASS:StatementSetString(stmt, index, value)
+	error("Function DatabaseAdapter::StatementSetString has not been implemented.")
+end
+
+
+
+
+
+--- @param stmt PreparedStatement
+--- @param callback fun(err:string|nil,data:table[]|nil)
+function CLASS:StatementSubmit(stmt, callback)
+	error("Function DatabaseAdapter::StatementSubmit has not been implemented.")
+end
+
+
 
 
 
