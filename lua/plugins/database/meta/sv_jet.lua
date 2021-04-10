9946980cf47e549526897105470710b46221c69d
Tassilo <tassia710@gmail.com>
2021-04-10 20:06:01 +0200

    Update sv_jet.lua

--- lua/plugins/database/meta/sv_jet.lua
+++ lua/plugins/database/meta/sv_jet.lua
@@ -18,3 +18,27 @@ function META:Database(name)
 	-- TODO
 	return nil
 end
+
+
+
+
+
+--- Creates a new MySQLOO database connection.
+---
+--- @param hostname string the database server's hostname
+--- @param port number the database server's port
+--- @param database string the database name
+--- @param username string the username
+--- @param password string the password
+--- @return Database the database
+---
+function META:CreateMySQLOODatabase(hostname, port, database, username, password)
+	local db = mysqloo.connect(hostname, username, password, database, port)
+	local adapter = setmetatable({
+		_Internal = db
+	}, debug.getregistry()["Jet:DatabaseAdapter:MySQLOO"])
+	return setmetatable({
+		_Adapter = adapter,
+		_Tables = {}
+	}, debug.getregistry()["Jet:Database"])
+end
