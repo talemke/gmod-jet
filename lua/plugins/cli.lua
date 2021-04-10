5683dff359269b3f5e50ae1f908d25269f4326ad
Tassilo <tassia710@gmail.com>
2021-04-10 15:15:53 +0200

    Create cli.lua

---
+++ lua/plugins/cli.lua
@@ -0,0 +1,20 @@
+--[[
+	CLI Plugin - (c) 2021 Tassilo <https://tassia.net>
+	Licensed under the MIT License.
+--]]
+
+return {
+
+	GroupID = "jet",
+	PluginID = "cli",
+	Version = "1.0.0",
+	Name = "Command-line Interface",
+	Description = "Adds a useful command-line interface for Jet.",
+	Authors = { "Tassilo" },
+
+	Homepage = "https://github.com/TASSIA710/Jet#readme",
+	License = "MIT",
+	Bugs = "https://github.com/TASSIA710/Jet/issues",
+	Repository = "https://github.com/TASSIA710/Jet.git",
+
+}
