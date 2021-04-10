638216e0c43174cfded9d354d1b3bc6c53ff1566
Tassilo <tassia710@gmail.com>
2021-04-10 18:59:21 +0200

    Update sv_Table.lua

--- lua/plugins/database/classes/sv_Table.lua
+++ lua/plugins/database/classes/sv_Table.lua
@@ -9,6 +9,50 @@
 local CLASS = {}
 CLASS.__index = CLASS
 
+--- @type Database
+CLASS._Database = nil
+
+--- @type string
+CLASS._LuaName = nil
+
+--- @type string
+CLASS._SQLName = nil
+
+--- @type string
+CLASS._SQLNameEscaped = nil
+
+--- @type TableColumn[]
+CLASS._Columns = nil
+
+
+
+
+
+function CLASS:CreateTable()
+	-- TODO
+end
+
+
+
+
+
+--- @return PreparedStatement
+function CLASS:DropTableQuery()
+	return self._Database:Prepare("DROP TABLE " .. self._SQLNameEscaped .. ";")
+end
+
+
+--- @param callback fun(err:string|nil)
+function CLASS:DropTable(callback)
+	return self:DropTableQuery():SubmitAsync(callback)
+end
+
+
+--- @return string|nil error
+function CLASS:DropTableBlocking()
+	return self:DropTableQuery():SubmitBlocking()
+end
+
 
 
 
