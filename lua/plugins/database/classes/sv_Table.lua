b087bb2ff52c69c999ed5acac378ce32ac7f3695
Tassilo <tassia710@gmail.com>
2021-04-11 16:31:12 +0200

    Update sv_Table.lua

--- lua/plugins/database/classes/sv_Table.lua
+++ lua/plugins/database/classes/sv_Table.lua
@@ -28,8 +28,43 @@ CLASS._Columns = nil
 
 
 
-function CLASS:CreateTable()
+--- @param raw table
+--- @return table
+function CLASS:BuildSQLRow(raw)
 	-- TODO
+	return raw
+end
+
+
+--- @param raw table
+--- @return table
+function CLASS:BuildSQLResult(raw)
+	local result = {}
+	for index, value in ipairs(raw) do
+		result[index] = self:BuildSQLRow(value)
+	end
+	return result
+end
+
+
+
+
+
+--- @return PreparedStatement
+function CLASS:CreateTableQuery()
+	-- TODO
+end
+
+
+--- @param callback fun(err:string|nil)
+function CLASS:CreateTable(callback)
+	return self:CreateTableQuery():SubmitAsync(callback)
+end
+
+
+--- @return string|nil error
+function CLASS:CreateTableBlocking()
+	return self:CreateTableQuery():SubmitBlocking()
 end
 
 
@@ -58,9 +93,35 @@ end
 
 
 --- @return PreparedStatement
+function CLASS:SelectAllQuery()
+	return self._Database:Prepare("SELECT * FROM " .. self._SQLNameEscaped .. ";")
+end
+
+
+--- @param callback fun(err:string|nil,data:table|nil)
+function CLASS:SelectAll(callback)
+	return self:DropTableQuery():SubmitAsync(function(err, data)
+		callback(err, letNN(data, function(it) return self:BuildSQLResult(it) end))
+	end)
+end
+
+
+--- @return string|nil error
+--- @return table|nil data
+function CLASS:SelectAllBlocking()
+	local err, data = self:CountAllQuery():SubmitBlocking()
+	return err, letNN(data, function(it) return self:BuildSQLResult(it) end)
+end
+
+
+
+
+
+--- @return PreparedStatement
 function CLASS:CountAllQuery()
 	return self._Database:Prepare("SELECT COUNT(*) FROM " .. self._SQLNameEscaped .. ";")
 end
+
 
 --- @param callback fun(err:string|nil,count:number|nil)
 function CLASS:CountAll(callback)
@@ -68,6 +129,7 @@ function CLASS:CountAll(callback)
 		callback(err, letNN(data, function(it) return it["COUNT(*)"] end))
 	end)
 end
+
 
 --- @return string|nil error
 --- @return number|nil count
