0aeb7827c6e866613e815e06d7f71eff5934a043
Tassilo <tassia710@gmail.com>
2021-04-11 16:30:25 +0200

    Update sv_TableColumn.lua

--- lua/plugins/database/classes/sv_TableColumn.lua
+++ lua/plugins/database/classes/sv_TableColumn.lua
@@ -9,11 +9,27 @@
 local CLASS = {}
 CLASS.__index = CLASS
 
+
+
+
+
 --- @type string
 CLASS._LuaName = nil
 
 --- @type string
 CLASS._SQLName = nil
+
+--- @type string
+CLASS._SQLType = nil
+
+--- @type boolean
+CLASS._SQLNullable = nil
+
+--- @type boolean
+CLASS._SQLHasDefault = nil
+
+--- @type nil|boolean|number|string
+CLASS._SQLDefault = nil
 
 
 
